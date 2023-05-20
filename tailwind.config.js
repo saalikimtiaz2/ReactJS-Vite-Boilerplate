46ccdb7028850897252fb10d9b020a4c8ea3bc24
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-05-20 15:14:18 +0400

    plugins added to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '250px',
+      sm: '768px',
+      md: '1024px',
+      lg: '1200px',
+      xl: '1336px',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
 }
